9ec1065372a70b5d361f0ef3f83421146c615de3
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-12-12 17:27:00 +0500

    working

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello World';
+console.log(message);
+// Declaration
+var x = 10;
+var y = 20;
+var sum;
+var title = 'Codevolution';
+// Data Type
+var isBeginner = true;
+var total = 0;
+var name = 'Vishwas';
+var sentence = "My name is " + name + "\nI am a begginner in Typescript";
+console.log(sentence);
---
+++ main.ts
@@ -0,0 +1,20 @@
+export {}
+let message = 'Hello World';
+console.log(message);
+
+// Declaration
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'Codevolution';
+
+// Data Type
+let isBeginner :boolean = true;
+let total:number = 0;
+let name:string = 'Vishwas';
+
+let sentence: string = `My name is ${name}
+I am a begginner in Typescript`;
+console.log(sentence);
